e820e01d23ffd0f66284cf3e23b08c185260d5a6
rileygriem <rmg9650@nyu.edu>
2020-10-20 05:41:55 -0400

    First push of project
    check it out!

---
+++ Project1.js
@@ -0,0 +1,51 @@
+//Riley Griem
+//Project 1
+//Energetic
+
+let circleSize = 10
+let circleDelta = 50
+
+
+function setup () {
+  createCanvas(800,600)
+  background(255,75,255)
+}
+
+function draw () {
+  background(255,75,255)
+  
+
+    
+  blueStripes();
+  //}
+  
+  yellowCircles ()
+  
+  
+}
+
+function yellowCircles () {
+  fill(255,255,0)
+  noStroke()
+  ellipse (random(100,150),random(25,75),50,50)
+}
+
+function blueStripes () {
+  
+  stroke(0,255,255)
+  strokeWeight(10)
+  noFill()
+  
+  var i;
+  let targetSize = width+200
+  
+  
+  for(i=0; i*circleDelta+circleSize<targetSize; i+=1){
+    ellipse(width/2,height/2,circleSize+circleDelta*i,circleSize+circleDelta*i)
+  }
+  
+  
+  
+
+  
+}
