b0e9a5bb83a824e163945f343b3c1ed890556c4a
rileygriem <rmg9650@nyu.edu>
2020-10-26 03:15:39 -0400

    Circle Illusion
    added movement to the circles, commented out function blueStripes.

--- RileyGriem-Project1/Project-1.js
+++ RileyGriem-Project1/Project-1.js
@@ -4,11 +4,12 @@
 
 let circleSize = 10
 let circleDelta = 50
+let t = 0
 
 
 function setup () {
   createCanvas(800,600)
-  background(255,75,255)
+  background(0)
 }
 
 function draw () {
@@ -18,25 +19,24 @@ function draw () {
   //blueStripes();
   
   //yellowCircles ();
-  
+  t++;
 }
 
 //middle made the vibrancy of the colors not work the way I wanted them to
 function drawTargets(){
   //let targetMiddle = new Target([0,0],300,[255,0,0]);
-  let targetLeft = new Target([-5,0],300,[0,255,255]);
-  let targetRight = new Target([5,0],300,[255,75,255]);
+  let targetLeft = new Target([-5,0],width*3,[0,255,255], 10, 9);
+  let targetRight = new Target([5,0],width*3,[255,75,255], 10, 13);
 
   //targetMiddle.draw();
-  targetLeft.draw();
-  targetRight.draw();
+  targetLeft.draw(t);
+  targetRight.draw(t);
 
 }
 function yellowCircles () {
 
   if (mouseIsPressed) {
-  blueStripes(10,10);
-  //blueStripes(-10,-10);
+
   fill(255,255,0)
   noStroke()
   ellipse (random(100,150),random(25,75),50,50)
@@ -49,7 +49,7 @@ function yellowCircles () {
 // color input as an array of length 3 for RGB color values of the Target
 class Target{
 
-  constructor(offset, diameter, color){
+  constructor(offset, diameter, color, motionRadius, speed){
     this.offsetX = offset[0]
     this.offsetY = offset[1]
     this.diameter = diameter
@@ -57,9 +57,15 @@ class Target{
     this.colorG = color[1]
     this.colorB = color[2]
     this.strokeWeight = 10
+    this.x = null
+    this.y = null
+    this.motionRadius = motionRadius
+    this.speed = speed
   }
 
-  draw(){
+  draw(t){
+    this.x = this.motionRadius*Math.cos(t*this.speed/10)
+    this.y = this.motionRadius*Math.sin(t*this.speed/10)
     stroke(this.colorR,this.colorG,this.colorB)
     strokeWeight(this.strokeWeight)
     noFill()
@@ -67,7 +73,7 @@ class Target{
     let i;
     let targetSize = this.diameter
     for(i=0; i*circleDelta+circleSize<targetSize; i+=1){
-      ellipse(mouseX+this.offsetX,mouseY+this.offsetY,circleSize+circleDelta*i,circleSize+circleDelta*i)
+      ellipse(width/2+this.x+this.offsetX,height/2+this.y+this.offsetY,circleSize+circleDelta*i,circleSize+circleDelta*i)
     }
   }
 }
@@ -81,7 +87,7 @@ class Target{
 }*/
 
 
-function blueStripes (x,y) {
+/*function blueStripes (x,y) {
   
   stroke(0,255,255)
   strokeWeight(10)
@@ -98,7 +104,7 @@ function blueStripes (x,y) {
   }
 
   
-}
+}*/
 /*
 //my tutor teaching me how arrays work:
 
