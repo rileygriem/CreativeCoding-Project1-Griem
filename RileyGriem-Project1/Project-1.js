d696c42a7bf2c21046f920a949f0d290271a985f
rileygriem <rmg9650@nyu.edu>
2020-10-25 22:53:43 -0400

    Array
    changed background color, putting yellow circles aside for now, added class Target

--- RileyGriem-Project1/Project-1.js
+++ RileyGriem-Project1/Project-1.js
@@ -13,18 +13,30 @@ function setup () {
 
 function draw () {
 
-  background(255,75,255)
-    
+  background(0,0,0)
+  drawTargets();
   //blueStripes();
   
-  yellowCircles ();
+  //yellowCircles ();
   
 }
 
+//middle made the vibrancy of the colors not work the way I wanted them to
+function drawTargets(){
+  //let targetMiddle = new Target([0,0],300,[255,0,0]);
+  let targetLeft = new Target([-5,0],300,[0,255,255]);
+  let targetRight = new Target([5,0],300,[255,75,255]);
+
+  //targetMiddle.draw();
+  targetLeft.draw();
+  targetRight.draw();
+
+}
 function yellowCircles () {
 
   if (mouseIsPressed) {
-  blueStripes();
+  blueStripes(10,10);
+  //blueStripes(-10,-10);
   fill(255,255,0)
   noStroke()
   ellipse (random(100,150),random(25,75),50,50)
@@ -32,7 +44,44 @@ function yellowCircles () {
   }
 }
 
-function blueStripes () {
+// center input as an array of integer x and y position of the center of the Target object
+// radius input as an integer representing the radius of the Target
+// color input as an array of length 3 for RGB color values of the Target
+class Target{
+
+  constructor(offset, diameter, color){
+    this.offsetX = offset[0]
+    this.offsetY = offset[1]
+    this.diameter = diameter
+    this.colorR = color[0]
+    this.colorG = color[1]
+    this.colorB = color[2]
+    this.strokeWeight = 10
+  }
+
+  draw(){
+    stroke(this.colorR,this.colorG,this.colorB)
+    strokeWeight(this.strokeWeight)
+    noFill()
+    
+    let i;
+    let targetSize = this.diameter
+    for(i=0; i*circleDelta+circleSize<targetSize; i+=1){
+      ellipse(mouseX+this.offsetX,mouseY+this.offsetY,circleSize+circleDelta*i,circleSize+circleDelta*i)
+    }
+  }
+}
+
+//made into a class instead
+/*function target(center, radius, color){
+
+  this.centerX = center[0]
+  this.centerY = center[0]
+  center == [x, y]
+}*/
+
+
+function blueStripes (x,y) {
   
   stroke(0,255,255)
   strokeWeight(10)
@@ -45,8 +94,34 @@ function blueStripes () {
   
   
   for(i=0; i*circleDelta+circleSize<targetSize; i+=1){
-    ellipse(mouseX,mouseY,circleSize+circleDelta*i,circleSize+circleDelta*i)
+    ellipse(x+mouseX,y+mouseY,circleSize+circleDelta*i,circleSize+circleDelta*i)
   }
 
   
 }
+/*
+//my tutor teaching me how arrays work:
+
+//groceryList
+//[0]turkey
+//[1]bread
+//[2]mayo
+food = ["turkey", "mayo"]
+food1 = "turkey"
+food2 = "mayo"
+food[0]
+food[1]
+print(food1+" "+food2)
+print(food1 == groceryList[0])
+// true
+groceryList = ["turkey","bread","mayo"]
+print("turkey"+" "+"mayo")
+// turkey mayo
+print(groceryList[0]+" "+groceryList[2])
+// turkey mayo
+
+print("turkey"+" happy")
+// turkey happy
+print(groceryList[0]+" happy")
+// 
+*/
